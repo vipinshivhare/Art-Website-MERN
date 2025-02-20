f33738056b95b8f4383f45b94dfe4ac04a284a46
vipinshivhare <vipinshivhare804@gmail.com>
2025-02-21 02:29:29 +0530

    re-direct issue fix

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe"
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async (req, res) => {
     // const frontend_url = "http://localhost:5173/";
-    const frontend_url = "https://bulbulart.netlify.app/myorders";
+    const frontend_url = "https://bulbulart.netlify.app";
 
     try {
         const newOrder = new orderModel({
