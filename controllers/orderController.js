2bc99330f0e261d161c320c6afa42b1e40aeace5
vipinshivhare <vipinshivhare804@gmail.com>
2025-02-21 02:24:18 +0530

    myorders fix

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,7 +4,8 @@ import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async (req, res) => {
-    const frontend_url = "http://localhost:5173/";
+    // const frontend_url = "http://localhost:5173/";
+    const frontend_url = "https://bulbulart.netlify.app/myorders";
 
     try {
         const newOrder = new orderModel({
@@ -42,8 +43,10 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: 'payment',
-            success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+            // success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
+            success_url: `${frontend_url}`,
+            // cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
+            cancel_url: `${frontend_url}`
         });
 
         res.json({ success: true, session_url: session.url });
