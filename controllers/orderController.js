c4414adadfe796baa25d8579e7e4ffb0087c4519
vipinshivhare <vipinshivhare804@gmail.com>
2025-02-22 17:11:35 +0530

    myorders navigation

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe"
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async (req, res) => {
     // const frontend_url = "http://localhost:5173/";
-    const frontend_url = "https://bulbulart.netlify.app";
+    const frontend_url = "https://bulbulart.netlify.app/myorders";
 
     try {
         const newOrder = new orderModel({
