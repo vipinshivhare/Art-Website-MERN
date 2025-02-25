02fc8aa61709ab350339bc1baf698b639d7bb7f5
vipinshivhare <vipinshivhare804@gmail.com>
2025-02-25 16:24:58 +0530

    order success and failed fix

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -4,7 +4,7 @@ import Stripe from "stripe"
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 const placeOrder = async (req, res) => {
-    // const frontend_url = "http://localhost:5173/";
+    
     const frontend_url = "https://bulbulart.netlify.app/myorders";
 
     try {
@@ -43,10 +43,8 @@ const placeOrder = async (req, res) => {
         const session = await stripe.checkout.sessions.create({
             line_items: line_items,
             mode: 'payment',
-            // success_url: `${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-            success_url: `${frontend_url}`,
-            // cancel_url: `${frontend_url}/verify?success=false&orderId=${newOrder._id}`
-            cancel_url: `${frontend_url}`
+            success_url: `${frontend_url}?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${frontend_url}?success=false&orderId=${newOrder._id}`
         });
 
         res.json({ success: true, session_url: session.url });
